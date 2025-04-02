e2e8d924e212b4a4099cc1580bfda05f0b733f45
小城 <kojolumi@gamil.com>
2025-04-02 16:59:10 +0900

    test

---
+++ page-news.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Template Name: page-news
+ * Description: This is the template
+ */
+
+get_header();
+?>
+<!-- <?php
+/*******************************************
+ * archive
+ *******************************************/
+// テーマディレクトリ
+$theme_url = get_template_directory_uri();
+get_header();
+?> -->
+
+<main>
+    <div>
+        <p>お知らせ一覧ページです-page-news</p>
+    </div>
+</main>
+
+
+
+
+
+<?php
+    get_footer(); 
+?>
