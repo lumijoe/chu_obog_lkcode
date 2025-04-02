c816483b484d3f1dc5291657648fcef31f0d6912
小城 <kojolumi@gamil.com>
2025-04-02 16:11:54 +0900

    test

---
+++ page-about.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Template Name: archive
+ * Description: This is the template
+ */
+
+get_header();
+?>
+<!-- <?php
+/*******************************************
+ * archive
+ *******************************************/
+// テーマディレクトリ
+$theme_url = get_template_directory_uri();
+get_header();
+?> -->
+
+<main>
+    <div>
+        <p>お知らせ一覧ページです</p>
+    </div>
+</main>
+
+
+
+
+
+<?php
+    get_footer(); 
+?>
