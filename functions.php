5c12b7ed43c56349c187b1a6e870304bd6f58396
小城 <kojolumi@gamil.com>
2025-04-03 09:50:47 +0900

    logo size change

--- functions.php
+++ functions.php
@@ -11,6 +11,11 @@ function my_login_logo() { ?>
     <style type="text/css">
         #login h1 a, .login h1 a {
             background-image: url(<?php echo get_template_directory_uri(); ?>/images/logo.svg);
+            margin:0!important;
+            width:100%;
+        }
+        .login h1 a {
+            background-size:100%important;
         }
     </style>
 <?php }
