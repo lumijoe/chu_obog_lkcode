8ab55d22217c246a1274c7fb60833981a0d92b30
小城 <kojolumi@gamil.com>
2025-04-03 09:28:31 +0900

    test

--- functions.php
+++ functions.php
@@ -7,3 +7,11 @@
  *
  * @package custom_theme
  */
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_template_directory_uri(); ?>/images/logo.gif);
+        }
+    </style>
+<?php }
+add_action('login_enqueue_scripts', 'my_login_logo');
