0d2ac5c39b2f96fe85ce5ace47c30231eb61a3ab
joe <spacedesign63@gmail.com>
2025-04-04 15:48:57 +0900

    add obog

---
+++ taxonomy-newscategory-obog.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<h1>OBOG会だより（<?php single_term_title(); ?>）のニュース一覧</h1>
+
+<?php if (have_posts()) : ?>
+    <ul>
+        <?php while (have_posts()) : the_post(); ?>
+            <li>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <p><?php the_category(', '); ?></p>
+            </li>
+        <?php endwhile; ?>
+    </ul>
+    <?php the_posts_pagination(); ?>
+<?php else : ?>
+    <p>会員だよりの投稿はありません。</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
