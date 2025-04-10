f55638c55fb15c3c0877a389d38b92af31d1898a
小城 <kojolumi@gamil.com>
2025-04-10 13:36:43 +0900

    ogp meta

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex">
-    <title>OBOGクラブ</title>
+    <meta name="description" content="中外炉工業株式会社の退職者コミュニティーです">
+    <title>中外炉工業OBOGクラブ</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- OGP Meta　Tags -->
     <meta property="og:title" content="中外炉工業OBOGクラブ">
