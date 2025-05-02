a74742cc3997f96b49ea0181f39b20162de6eec3
joe <spacedesign63@gmail.com>
2025-05-02 18:24:17 +0900

    test

--- home.php
+++ home.php
@@ -178,19 +178,23 @@ get_header();
   <h2 class="section-ttl">OBOGの皆さまへ</h2>
   <div class="l-pagebanner-inner grid-container2">
     <div class="grid-item">
-      <div class="banner-white">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/home/img_home_bgwhite.png" alt="" class="is-background">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/home/icon_mail_blue.svg" alt="" class="is-fronticon">
-      </div>
+      <a href="<?php echo home_url('/about#memberpost'); ?>">
+        <div class="banner-white">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/home/img_home_bgwhite.png" alt="" class="is-background">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/home/icon_note_blue.svg" alt="" class="is-fronticon">
+        </div>
+      </a>
       <h2>ご入稿について（会員限定）<br><span class="is-figspan">サイト内の「OBOG会だより」や「会員だより」でご紹介する原稿を募集しています。OBOG会の予定案内や開催後の楽しいレポートのほか、ぜひ皆さまの近況報告や随想などをご入稿ください。</span></h2>
     </div>
-    <figure class="grid-item">
+    <div class="grid-item">
       <a href="<?php echo get_template_directory_uri(); ?>/images/home/chugairo_print.pdf" target="_blank">
-        <img src="https://dummyimage.com/349x198/a4a4a4/fff.jpg" alt="" srcset="" class="is-wide">
-        <figcaption>弔事のご連絡について<br><span class="is-figspan">弔事のご連絡は、記入様式を印刷し、各項目にご記入のうえFAXでご連絡願います。なお、土日祝日は担当者不在につき、ご対応が遅れる場合もあることを、予めご了承願います。</span></figcaption>
+        <div class="banner-white">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/home/img_home_bgwhite.png" alt="" class="is-background">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/home/icon_mail_blue.svg" alt="" class="is-fronticon">
+        </div>
       </a>
-    </figure>
-
+      <h2>弔事のご連絡について<br><span class="is-figspan">弔事のご連絡は、記入様式を印刷し、各項目にご記入のうえFAXでご連絡願います。なお、土日祝日は担当者不在につき、ご対応が遅れる場合もあることを、予めご了承願います。</span></h2>
+    </div>
   </div>
 </section>
 
