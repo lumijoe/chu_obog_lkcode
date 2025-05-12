85344cbde984df6a8d730905f0a664fff3f05a3f
小城 <kojolumi@gamil.com>
2025-05-12 11:18:15 +0900

    test

--- taxonomy-newscategory-allevent.php
+++ taxonomy-newscategory-allevent.php
@@ -37,62 +37,26 @@
             <ul>
                 <?php while (have_posts()) : the_post(); ?>
                     <li>
+                        <!-- ACF投稿日時 -->
+                        <date><?php echo get_the_date('Y.m.d'); ?></date>
+
+                        <!-- カテゴリ表示 -->
+                        <?php
+                        $terms = get_the_terms(get_the_ID(), 'newscategory');
+                        if ($terms && !is_wp_error($terms)) :
+                            $first_term = $terms[0];
+                            $category_output = $first_term->name;
+                        else :
+                            $category_output = 'カテゴリなし';
+                        endif;
+                        ?>
+                        <p class="item-category"><?php echo esc_html($category_output); ?></p>
+
                         <!-- ACFタイトル -->
                         <?php if (get_field('post_title')) : ?>
                             <a href="<?php the_permalink(); ?>">TEST投稿：<?php the_field('post_title'); ?></a>
                         <?php endif; ?>
 
-                        <!-- ACF投稿日時 -->
-                        <p><?php echo get_the_date('Y年m月d日'); ?></p>
-
-                        <!-- カテゴリ表示 -->
-                        <?php
-                        // 投稿が所属しているタクソノミー 'newscategory' のカテゴリを取得
-                        $terms = get_the_terms(get_the_ID(), 'newscategory');
-                        if ($terms && !is_wp_error($terms)) : ?>
-                            <p class="post-category">
-                                <?php
-                                $term_list = array();
-                                foreach ($terms as $term) {
-                                    $term_list[] = $term->name; // カテゴリ名を取得
-                                }
-                                echo implode(', ', $term_list); // 複数カテゴリがあればカンマ区切りで表示
-                                ?>
-                            </p>
-                        <?php endif; ?>
-
-                        <!-- ACF本文 -->
-                        <?php if (get_field('post_text')) : ?>
-                            <p><?php the_field('post_text'); ?></p>
-                        <?php endif; ?>
-
-                        <!-- 画像 -->
-                        <?php if (get_field('post_image')) : ?>
-                            <img src="<?php the_field('post_image'); ?>"
-                                alt="ニュース画像"
-                                width="300"
-                                data-src="<?php the_field('post_image'); ?>"
-                                decoding="async"
-                                class="lazyloaded">
-                        <?php endif; ?>
-
-                        <!-- PDF -->
-                        <?php if (get_field('post_pdf') && get_field('post_pdf_title')) : ?>
-                            <p class="pdf-title">
-                                <a href="<?php the_field('post_pdf'); ?>" target="_blank" rel="noopener noreferrer">
-                                    <?php the_field('post_pdf_title'); ?>
-                                </a>
-                            </p>
-                        <?php endif; ?>
-
-                        <!-- 外部リンク -->
-                        <?php if (get_field('post_url')) : ?>
-                            <p class="post-url">
-                                <a href="<?php the_field('post_url'); ?>" target="_blank" rel="noopener noreferrer">
-                                    <?php the_field('post_url'); ?>
-                                </a>
-                            </p>
-                        <?php endif; ?>
                         <hr>
                     </li>
                 <?php endwhile; ?>
